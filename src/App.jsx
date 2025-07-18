221fd5567e429528b91e0abbff25a077817c1d2f
Deva Pramod <112617358+DevaP11@users.noreply.github.com>
2025-07-18 09:49:56 +0530

    Tags

--- src/App.jsx
+++ src/App.jsx
@@ -45,64 +45,73 @@ const bookmarkList = [
 ]
 
 function App () {
-  const [activeTab, setActiveTab] = useState('bookmarks')
+  const tags = ['Campaigns', 'Movies']
+
+  const [activeTab, setActiveTab] = useState(tags[0])
   const handleTabChange = (value) => {
     setActiveTab(value)
   }
 
-  const isTabBookmarks = activeTab === 'bookmarks'
   return (
     <main className='grid grid-cols-1 gap-4 bg-background text-foreground mt-4 place-self-center max-w-[90vw]'>
       <Tabs value={activeTab} onValueChange={handleTabChange} className='place-self-center mt-8'>
         <div className='flex flex-row  mb-0 w-full justify-between'>
           <TabsList className='bg-stone-200'>
-            <TabsTrigger value='home' className='font-extralight bg-stone-200 data-[state=active]:bg-stone-300 data-[state=active]:shadow-md'>Home</TabsTrigger>
-            <TabsTrigger value='bookmarks' className='font-extralight bg-stone-200 data-[state=active]:bg-stone-300 data-[state=active]:shadow-md'>Bookmarks</TabsTrigger>
-          </TabsList>
-          <div className='align-middle'>
-            {isTabBookmarks &&
-              <Button
-                type='icon'
-                className='bg-stone-200 hover:bg-stone-100 text-black shadow-none mr-2 h-8 w-30  font-extralight'
-                onClick={(e) => {
-                  e.stopPropagation()
-                }}
-              >
-                <Search /> Search
-              </Button>}
-            {isTabBookmarks &&
-              <Button
-                type='icon'
-                className='bg-stone-300 hover:bg-stone-200 text-black shadow-none mr-2 h-8  font-extralight'
-                onClick={(e) => {
-                  e.stopPropagation()
-                }}
-              >
-                <Plus /> Add
-              </Button>}
-          </div>
-        </div>
-        <TabsContent value='home' className='w-[86vw]'>
-          <p>Home</p>
-        </TabsContent>
-        <TabsContent value='bookmarks' className='w-[86vw]'>
-          <div className='grid grid-cols-4 grid-rows-4 gap-4 h-screen'>
             {
-              bookmarkList?.map((bookmarkItem, index) => {
+              tags?.map((t, i) => {
                 return (
-                  <Bookmark
-                    key={index}
-                    id={index}
-                    title={bookmarkItem?.title}
-                    description={bookmarkItem?.description}
-                    image={bookmarkItem?.image}
-                    size={bookmarkItem?.size}
-                  />
+                  <TabsTrigger key={`tags_trigger_${i}`} value={t} className='font-extralight bg-stone-200 data-[state=active]:bg-stone-300 data-[state=active]:shadow-md'>
+                    {t}
+                  </TabsTrigger>
                 )
               })
             }
+          </TabsList>
+          <div className='align-middle'>
+            <Button
+              type='icon'
+              className='bg-stone-200 hover:bg-stone-100 text-black shadow-none mr-2 h-8 w-30  font-extralight'
+              onClick={(e) => {
+                e.stopPropagation()
+              }}
+            >
+              <Search /> Search
+            </Button>
+            <Button
+              type='icon'
+              className='bg-stone-300 hover:bg-stone-200 text-black shadow-none mr-2 h-8  font-extralight'
+              onClick={(e) => {
+                e.stopPropagation()
+              }}
+            >
+              <Plus /> Add
+            </Button>
           </div>
-        </TabsContent>
+        </div>
+        {
+          tags.map((t, i) => {
+            return (
+              <TabsContent key={`tags_content_${i}`} value={t} className='w-[86vw]'>
+                <div className='grid grid-cols-4 grid-rows-4 gap-4 h-screen'>
+                  {
+                    bookmarkList?.map((bookmarkItem, index) => {
+                      return (
+                        <Bookmark
+                          key={index}
+                          id={index}
+                          title={bookmarkItem?.title}
+                          description={bookmarkItem?.description}
+                          image={bookmarkItem?.image}
+                          size={bookmarkItem?.size}
+                        />
+                      )
+                    })
+                  }
+                </div>
+              </TabsContent>
+            )
+          })
+        }
       </Tabs>
     </main>
   )
